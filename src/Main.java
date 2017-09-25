cf50b1bec7d4d242fa61a3296d9c1f5c8b5611c7
jatp61219 <jatp61219@gmail.com>
2017-09-25 14:38:10 +0800

    ａａ

---
+++ src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("105024127陳詠中");// write your code here
+        System.out.println("資傳系");// write your code here
+    }
+}
